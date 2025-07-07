31fbb65a6571a6d89cb9dd3f8efa02d31ffbec83
Michal Wisniewski <michalwisniewskifrontend@onet.pl>
2025-07-07 20:03:26 +0200

    complete frontend

--- src/components/InvoicesAdmin.js
+++ src/components/InvoicesAdmin.js
@@ -26,6 +26,44 @@ const InvoicesAdmin = () => {
   const [orders, setOrders] = useState([])
   const [totalamount, setTotalAmount] = useState(0)
 
+  const [invoices, setInvoices] = useState([])
+
+
+
+  const [isForm2Visible, setIsForm2Visible] = useState(false)
+
+
+
+
+const [correctioninvoicenumber, setCorrectioninvoicenumber] = useState('')
+const [correctiondateofissue, setCorrectiondateofissue] = useState('')
+const [correctiondateofsale, setCorrectiondateofsale] = useState('')
+const [correctionsellercompanyname, setCorrectionsellercompanyname] = useState('')
+const [correctionsellercompanystreet, setCorrectionsellercompanystreet] = useState('')
+const [correctionsellercompanypostcode, setCorrectionsellercompanypostcode] = useState('')
+const [correctionsellercompanycity, setCorrectionsellercompanycity] = useState('')
+const [correctionsellercompanynip, setCorrectionsellercompanynip] = useState('')
+const [correctionsellercompanyregon, setCorrectionsellercompanyregon] = useState('')
+const [correctioncustomername, setCorrectioncustomername] = useState('')
+const [correctioncustomersurname, setCorrectioncustomersurname] = useState('')
+const [correctioncustomerstreet, setCorrectioncustomerstreet] = useState('')
+const [correctioncustomerpostcode, setCorrectioncustomerpostcode] = useState('')
+const [correctioncustomercity, setCorrectioncustomercity] = useState('')
+const [correctioncustomercompanyname, setCorrectioncustomercompanyname] = useState('')
+const [correctioncustomercompanystreet, setCorrectioncustomercompanystreet] = useState('')
+const [correctioncustomercompanypostcode, setCorrectioncustomercompanypostcode] = useState('')
+const [correctioncustomercompanycity, setCorrectioncustomercompanycity] = useState('')
+const [correctioncustomerinvoice, setCorrectioncustomerinvoice] = useState(false)
+const [correctioncustomernip, setCorrectioncustomernip] = useState('')
+const [correctioncustomerregon, setCorrectioncustomerregon] = useState('')
+const [correctionordercontent, setCorrectionordercontent] = useState('')
+const [correctionorderamount, setCorrectionorderamount] = useState('')
+const [correctionbasisofvatexemption, Setcorrectionbasisofvatexemption] = useState('')
+const [correctioninvoiceterm, Setcorrectioninvoiceterm] = useState('')
+const [correctionordertime, Setcorrectionordertime] = useState('')     
+const [correctionlogin, Setcorrectionlogin] = useState('') 
+const [correctionid, setCorrectionid] = useState('')
+
 
   
      useEffect(() => {
@@ -33,6 +71,14 @@ const InvoicesAdmin = () => {
         .then((response) => setTaxdatas(response.data))
         .catch((err) => console.log('error fetching taxdatas, error: ' + err))
     }, [])
+
+    
+     useEffect(() => {
+        axios.get('http://localhost:5000/invoices')
+        .then((response) => setInvoices(response.data))
+        .catch((err) => console.log('error fetching invoices, error: ' + err))
+    }, [])
+
 
 
 
@@ -91,6 +137,8 @@ useEffect(() => {
     vatpercentage,
     basisforvatexemption
   })
+
+
     .then((response) => {
       setTaxdatas(taxdatas.map(td => td._id === editingId ? response.data : td));
       setSellerCompanyName('');
@@ -107,6 +155,131 @@ useEffect(() => {
     .catch((err) => console.error("Error updating taxdatas:", err));
 };
 
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'  // opcjonalnie płynne przewijanie
+  });
+}
+
+
+const handleEditInvoice = (
+  id,
+  invoicenumber,
+  invoicedateofissue,
+  dateofsale,
+  sellercompanyname,
+  sellercompanystreet,
+  sellercompanypostcode,
+  sellercompanycity,
+  sellercompanynip,
+  sellercompanyregon,
+  customername,
+  customersurname,
+  customerstreet,
+  customerpostcode,
+  customercity,
+  customercompanyname,
+  customercompanystreet,
+  customercompanypostcode,
+  customercompanycity,
+  customerinvoice,
+  customercompanynip,
+  customercompanyregon,
+  ordercontent,
+  orderamount,
+  basisforvatexemption,
+  paymentterm,
+  ordertime,
+  login
+
+
+
+
+
+
+
+) => {
+  setIsForm2Visible(true)
+  scrollToTop()
+
+  setCorrectionid(id)
+  setCorrectioninvoicenumber(invoicenumber)
+  setCorrectiondateofissue(invoicedateofissue)
+  setCorrectiondateofsale(dateofsale)
+  setCorrectionsellercompanyname(sellercompanyname)
+  setCorrectionsellercompanystreet(sellercompanystreet)
+  setCorrectionsellercompanypostcode(sellercompanypostcode)
+  setCorrectionsellercompanycity(sellercompanycity)
+  setCorrectionsellercompanynip(sellercompanynip)
+  setCorrectionsellercompanyregon(sellercompanyregon)
+  setCorrectioncustomername(customername)
+  setCorrectioncustomersurname(customersurname)
+  setCorrectioncustomerstreet(customerstreet)
+  setCorrectioncustomerpostcode(customerpostcode)
+  setCorrectioncustomercity(customercity)
+  setCorrectioncustomercompanyname(customercompanyname)
+  setCorrectioncustomercompanystreet(customercompanystreet)
+  setCorrectioncustomercompanypostcode(customercompanypostcode)
+  setCorrectioncustomercompanycity(customercompanycity)
+  setCorrectioncustomerinvoice(customerinvoice)
+  setCorrectioncustomernip(customercompanynip)
+  setCorrectioncustomerregon(customercompanyregon)
+  setCorrectionordercontent(ordercontent)
+  setCorrectionorderamount(orderamount)
+  Setcorrectionbasisofvatexemption(basisforvatexemption)
+  Setcorrectioninvoiceterm(paymentterm)
+  Setcorrectionordertime(ordertime)
+  Setcorrectionlogin(login)
+}
+
+
+
+const handleSubmit2 = () => {
+
+
+ axios.put(`http://localhost:5000/invoices/${correctionid}`, {
+invoicenumber: correctioninvoicenumber,
+  invoicedateofissue: correctiondateofissue,
+  dateofsale: correctiondateofsale,
+  sellercompanyname: correctionsellercompanyname,
+  sellercompanystreet: correctionsellercompanystreet,
+  sellercompanypostcode: correctionsellercompanypostcode,
+  sellercompanycity: correctionsellercompanycity,
+  sellercompanynip: correctionsellercompanynip,
+  sellercompanyregon: correctionsellercompanyregon,
+  customername: correctioncustomername,
+  customersurname: correctioncustomersurname,
+  customerstreet: correctioncustomerstreet,
+  customerpostcode: correctioncustomerpostcode,
+  customercity: correctioncustomercity,
+  customercompanyname: correctioncustomercompanyname,
+  customercompanystreet: correctioncustomercompanystreet,
+  customercompanypostcode: correctioncustomercompanypostcode,
+  customercompanycity: correctioncustomercompanycity,
+  customerinvoice: correctioncustomerinvoice,
+  customercompanynip: correctioncustomernip,
+  customercompanyregon: correctioncustomerregon,
+  ordercontent: correctionordercontent,
+  orderamount: correctionorderamount,
+  basisforvatexemption: correctionbasisofvatexemption,
+  paymentterm: correctioninvoiceterm,
+  ordertime: correctionordertime,
+  login: correctionlogin
+
+ }) 
+   .then((response) => {
+      setInvoices(prev =>
+        prev.map(inv => inv._id === correctionid ? response.data : inv)
+      );
+
+      // Czyścimy formularz i chowamy
+      setIsForm2Visible(false);
+    })
+ .catch((err) => console.error("Error updating invoices:", err));
+
+  setIsForm2Visible(false)
+}
 
  
 
@@ -152,6 +325,291 @@ useEffect(() => {
             <div>
                 <h3 className="adminInfo1" style={{color: 'black'}}>Aktualny obrót w roku {new Date().getFullYear().toString()} to {totalamount} zł. Jeśli przekroczy 200 000 zł zmień stawkę VAT z 0 na 23% i ustaw podstawę zwolnienia z VAT na pusty string lub znak "-". </h3>
             </div>
+            {isForm2Visible ? (
+            <div className="editTaxDatasWrapper editInvoiceDataWrapper">
+            <form onSubmit={handleSubmit2}>
+              <h1 style={{color: 'black'}}>Formularz korekcji faktury: </h1>
+              <label>numer faktury: 
+  <input 
+    type="text" 
+    value={correctioninvoicenumber} 
+    onChange={(e) => setCorrectioninvoicenumber(e.target.value)} 
+  />
+</label>
+
+<label>data wystawienia:
+  <input 
+    type="text" 
+    value={correctiondateofissue} 
+    onChange={(e) => setCorrectiondateofissue(e.target.value)} 
+  />
+</label>
+
+<label>data sprzedaży:
+  <input 
+    type="text" 
+    value={correctiondateofsale} 
+    onChange={(e) => setCorrectiondateofsale(e.target.value)} 
+  />
+</label>
+
+<label>nazwa firmy sprzedającego: 
+  <input 
+    type="text" 
+    value={correctionsellercompanyname} 
+    onChange={(e) => setCorrectionsellercompanyname(e.target.value)} 
+  />
+</label>
+
+<label>adres firmy sprzedającego: 
+  <input 
+    type="text" 
+    value={correctionsellercompanystreet} 
+    onChange={(e) => setCorrectionsellercompanystreet(e.target.value)} 
+  />
+</label>
+
+<label>kod pocztowy firmy sprzedającego: 
+  <input 
+    type="text" 
+    value={correctionsellercompanypostcode} 
+    onChange={(e) => setCorrectionsellercompanypostcode(e.target.value)} 
+  />
+</label>
+
+<label>miasto firmy sprzedającego:
+  <input 
+    type="text" 
+    value={correctionsellercompanycity} 
+    onChange={(e) => setCorrectionsellercompanycity(e.target.value)} 
+  />
+</label>
+
+<label>NIP firmy sprzedającego: 
+  <input 
+    type="text" 
+    value={correctionsellercompanynip} 
+    onChange={(e) => setCorrectionsellercompanynip(e.target.value)} 
+  />
+</label>
+
+<label>REGON firmy sprzedającego:
+  <input 
+    type="text" 
+    value={correctionsellercompanyregon} 
+    onChange={(e) => setCorrectionsellercompanyregon(e.target.value)} 
+  />
+</label>
+
+<label>imię klienta:
+  <input 
+    type="text" 
+    value={correctioncustomername} 
+    onChange={(e) => setCorrectioncustomername(e.target.value)} 
+  />
+</label>
+
+<label>nazwisko klienta:
+  <input 
+    type="text" 
+    value={correctioncustomersurname} 
+    onChange={(e) => setCorrectioncustomersurname(e.target.value)} 
+  />
+</label>
+
+<label>adres klienta ulica nr domu, nr mieszkania:
+  <input 
+    type="text" 
+    value={correctioncustomerstreet} 
+    onChange={(e) => setCorrectioncustomerstreet(e.target.value)} 
+  />
+</label>
+
+<label>kod pocztowy klienta:
+  <input 
+    type="text" 
+    value={correctioncustomerpostcode} 
+    onChange={(e) => setCorrectioncustomerpostcode(e.target.value)} 
+  />
+</label>
+
+<label>miasto klienta:
+  <input 
+    type="text" 
+    value={correctioncustomercity} 
+    onChange={(e) => setCorrectioncustomercity(e.target.value)} 
+  />
+</label>
+
+<label>nazwa firmy klienta:
+  <input 
+    type="text" 
+    value={correctioncustomercompanyname} 
+    onChange={(e) => setCorrectioncustomercompanyname(e.target.value)} 
+  />
+</label>
+
+<label>adres firmy klienta:
+  <input 
+    type="text" 
+    value={correctioncustomercompanystreet} 
+    onChange={(e) => setCorrectioncustomercompanystreet(e.target.value)} 
+  />
+</label>
+
+<label>kod pocztowy firmy klienta:
+  <input 
+    type="text" 
+    value={correctioncustomercompanypostcode} 
+    onChange={(e) => setCorrectioncustomercompanypostcode(e.target.value)} 
+  />
+</label>
+
+<label>miasto firmy klienta:
+  <input 
+    type="text" 
+    value={correctioncustomercompanycity} 
+    onChange={(e) => setCorrectioncustomercompanycity(e.target.value)} 
+  />
+</label>
+
+<label>Czy chce fakturę?:
+  <input 
+    type="text" 
+    value={correctioncustomerinvoice} 
+    onChange={(e) => setCorrectioncustomerinvoice(e.target.value === 'true')} 
+  />
+</label>
+
+<label>NIP firmy klienta:
+  <input 
+    type="text" 
+    value={correctioncustomernip} 
+    onChange={(e) => setCorrectioncustomernip(e.target.value)} 
+  />
+</label>
+
+<label>REGON firmy klienta:
+  <input 
+    type="text" 
+    value={correctioncustomerregon} 
+    onChange={(e) => setCorrectioncustomerregon(e.target.value)} 
+  />
+</label>
+
+<label>Treść zamówienia: 
+  <input 
+    type="text" 
+    value={correctionordercontent} 
+    onChange={(e) => setCorrectionordercontent(e.target.value)} 
+  />
+</label>
+
+<label>Kwota zamówienia brutto:
+  <input 
+    type="text" 
+    value={correctionorderamount} 
+    onChange={(e) => setCorrectionorderamount(e.target.value)} 
+  />
+</label>
+
+<label>Podstawa zwolnienia z VAT:
+  <input 
+    type="text" 
+    value={correctionbasisofvatexemption} 
+    onChange={(e) => Setcorrectionbasisofvatexemption(e.target.value)} 
+  />
+</label>
+
+<label>Termin płatności:
+  <input 
+    type="text" 
+    value={correctioninvoiceterm} 
+    onChange={(e) => Setcorrectioninvoiceterm(e.target.value)} 
+  />
+</label>
+
+<label>Czas zamówienia:
+  <input 
+    type="text" 
+    value={correctionordertime} 
+    onChange={(e) => Setcorrectionordertime(e.target.value)} 
+  />
+</label>
+
+<label>Login:
+  <input 
+    type="text" 
+    value={correctionlogin} 
+    onChange={(e) => Setcorrectionlogin(e.target.value)} 
+  />
+</label>
+
+
+              <button className="buttonToEdit">Skoryguj</button>
+            </form>
+            </div>
+            ) : null}
+            <div className="invoicesAdminPresentationList">
+              <h1>Lista faktur: </h1>
+              {invoices.map(invoice => (
+              <div className="singleInvoice">
+                <p><strong>numer faktury: </strong> {invoice.invoicenumber}</p>
+                <p><strong>data wystawienia: </strong>{invoice.invoicedateofissue}</p>
+                <p><strong>data sprzedaży: </strong>{invoice.dateofsale}</p>
+                <p><strong>nazwa firmy sprzedającego: </strong>{invoice.sellercompanyname}</p>
+                <p><strong>adres firmy sprzedającego: </strong>{invoice.sellercompanystreet}</p>
+                <p><strong>kod pocztowy firmy sprzedającego: </strong>{invoice.sellercompanypostcode}</p>
+                <p><strong>miasto firmy sprzedającego: </strong>{invoice.sellercompanycity}</p>
+                <p><strong>NIP firmy sprzedającego: </strong>{invoice.sellercompanynip}</p>
+                <p><strong>REGON firmy sprzedającego: </strong>{invoice.sellercompanyregon}</p>
+                <p><strong>imię klienta: </strong>{invoice.customername}</p>
+                <p><strong>nazwisko klienta: </strong>{invoice.customersurname}</p>
+                <p><strong>adres klienta ulica nr domu, nr mieszkania: </strong>{invoice.customerstreet}</p>
+                <p><strong>kod pocztowy klienta: </strong>{invoice.customerpostcode}</p>
+                <p><strong>miasto klienta: </strong>{invoice.customercity}</p>
+                <p><strong>nazwa firmy klienta: </strong>{invoice.customercompanyname}</p>
+                <p><strong>adres firmy klienta: </strong>{invoice.customercompanystreet}</p>
+                <p><strong>kod pocztowy firmy klienta: </strong>{invoice.customercompanypostcode}</p>
+                <p><strong>miasto firmy klienta: </strong>{invoice.customercompanycity}</p>
+                <p><strong>Czy chce fakturę?: </strong>{invoice.customerinvoice? 'tak': 'nie'}</p>
+                <p><strong>NIP firmy klienta:</strong>{invoice.customercompanynip}</p>
+                <p><strong>REGON firmy klienta:</strong>{invoice.customercompanyregon}</p>
+             
+
+
+ <p><strong>Treść zamówienia:</strong> </p>
+
+               <table>
+  <thead>
+    <tr>
+      <th>Tytuł</th>
+      <th>Autor</th>
+      <th>Cena brutto</th>
+    </tr>
+  </thead>
+  <tbody>
+    {JSON.parse(invoice.ordercontent).map((item, index) => (
+      <tr key={index}>
+        <td>{item.title}</td>
+        <td>{item.author}</td>
+        <td>{item.price} zł</td>
+      </tr>
+    ))}
+  </tbody>
+</table>
+                
+                <p><strong>Kwota zamówienia brutto: </strong>{invoice.orderamount}</p>
+                <p><strong>Podstawa zwolnienia z VAT: </strong>{invoice.basisforvatexemption}</p>
+                <p><strong>Termin płatności: </strong> {invoice.paymentterm}</p>
+                <p><strong>Czas zamówienia: </strong>{invoice.ordertime}</p>
+                <p><strong>Login:</strong>{invoice.login}</p>
+                <button className="buttonToEdit" onClick={() => handleEditInvoice(invoice._id, invoice.invoicenumber, invoice.invoicedateofissue, invoice.dateofsale, invoice.sellercompanyname, invoice.sellercompanystreet, invoice.sellercompanypostcode, invoice.sellercompanycity, invoice.sellercompanynip, invoice.sellercompanyregon, invoice.customername, invoice.customersurname, invoice.customerstreet, invoice.customerpostcode, invoice.customercity, invoice.customercompanyname, invoice.customercompanystreet, invoice.customercompanypostcode, invoice.customercompanycity, invoice.customerinvoice, invoice.customercompanynip, invoice.customercompanyregon, invoice.ordercontent, invoice.orderamount, invoice.basisforvatexemption, invoice.paymentterm, invoice.ordertime, invoice.login)
+}>Koryguj fakturę</button>
+                <button className="buttonToEdit">Zobacz fakturę</button>
+              </div>
+              ))}
+            </div>
             
           </div>
         </div>
@@ -162,4 +620,4 @@ useEffect(() => {
   );
 };
 
-export default InvoicesAdmin;
+export default InvoicesAdmin
