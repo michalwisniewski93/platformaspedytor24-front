06898da3ae1ff4133233d2edc115410ae94d293e
Michał Wiśniewski Front End Developer with backend skills <michalwisniewskifrontend@onet.pl>
2025-08-12 21:01:27 +0200

    Update AboutP24.js

--- src/components/AboutP24.js
+++ src/components/AboutP24.js
@@ -1,1 +1,22 @@
+import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
+
+const AboutP24 = () => {
+    return (
+        <div className="app">
+        <Header/>
+        <div className="privacyPolicy">
+            <h1>Regulamin</h1>
+            <br/><br/>
+            <p>
+               AboutP24
+            </p>
+        </div>
+        <Footer/>
+        </div>
+    )
+}
+
+export default AboutP24
 
