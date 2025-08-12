baeb2cf40223d9f7148aa9b3e5e0b09d16dde691
Michał Wiśniewski Front End Developer with backend skills <michalwisniewskifrontend@onet.pl>
2025-08-12 21:00:37 +0200

    Update TermsAndConditions.js

--- src/components/TermsAndConditions.js
+++ src/components/TermsAndConditions.js
@@ -1,1 +1,21 @@
+import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
 
+const TermsAndConditions = () => {
+    return (
+        <div className="app">
+        <Header/>
+        <div className="privacyPolicy">
+            <h1>Regulamin</h1>
+            <br/><br/>
+            <p>
+               Terms and Conditions
+            </p>
+        </div>
+        <Footer/>
+        </div>
+    )
+}
+
+export default TermsAndConditions
