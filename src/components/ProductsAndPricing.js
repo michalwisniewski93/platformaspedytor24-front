93bd0a54e00197c27aa4d7dc94b2713aaf59138d
Michał Wiśniewski Front End Developer with backend skills <michalwisniewskifrontend@onet.pl>
2025-08-12 21:02:43 +0200

    Update ProductsAndPricing.js

--- src/components/ProductsAndPricing.js
+++ src/components/ProductsAndPricing.js
@@ -1,1 +1,21 @@
+import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
 
+const ProductsAndPricing = () => {
+    return (
+        <div className="app">
+        <Header/>
+        <div className="privacyPolicy">
+            <h1>Regulamin</h1>
+            <br/><br/>
+            <p>
+               ProductsAndPricing
+            </p>
+        </div>
+        <Footer/>
+        </div>
+    )
+}
+
+export default ProductsAndPricing
