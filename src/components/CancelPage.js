5d924fd0392ee637be723a87888ea7219b98355f
Michał Wiśniewski Front End Developer with backend skills <michalwisniewskifrontend@onet.pl>
2025-09-17 19:27:12 +0200

    Update CancelPage.js

--- src/components/CancelPage.js
+++ src/components/CancelPage.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 const CancelPage = () => {
-    return(
-        <h1>płatność nieudana</h1>
-    )
-}
+  useEffect(() => {
+    // Sprzątanie stanu płatności
+    sessionStorage.removeItem('paymentStarted');
+    // Nie usuwamy orderData, jeśli chcemy umożliwić wznowienie
+    // sessionStorage.removeItem('orderData');
+  }, []);
 
-export default CancelPage
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Płatność została anulowana</h1>
+      <p>Możesz wrócić do koszyka i spróbować ponownie.</p>
+      <Link to="/">Wróć na stronę główną</Link>
+    </div>
+  );
+};
+
+export default CancelPage;
+
