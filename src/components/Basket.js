6e16950664631fe2c4405e31ea10d36ddac03526
AKCode-eu <kontakt@akcode.eu>
2025-09-22 22:36:15 +0200

    Update Basket.js

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -137,9 +137,7 @@ const Basket = () => {
   // ===============================
   // Obliczanie ceny całkowitej
   // ===============================
-const totalPrice = 0.10;
-  
-
+const totalPrice = basket.reduce((sum, item) => sum + parseFloat(item.price || 0), 0);
 
   if (basket.length === 0) {
     return <p>Koszyk jest pusty. <Link to="/">Powrót do strony głównej</Link></p>;
