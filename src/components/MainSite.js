bf555a9aee4f58ec227b8539cbe7b8a12542605f
Michał Wiśniewski Front End Developer with backend skills <michalwisniewskifrontend@onet.pl>
2025-10-27 20:13:52 +0100

    Update MainSite.js

--- src/components/MainSite.js
+++ src/components/MainSite.js
@@ -12,6 +12,7 @@ const MainSite = () => {
 useEffect(() => {
     const referrer = document.referrer.toLowerCase();
     let source = null;
+    console.log("Referrer:", referrer);
 
     if (referrer.includes("google")) source = "Google";
     else if (referrer.includes("youtube")) source = "YouTube";
