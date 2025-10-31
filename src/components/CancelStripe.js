4bf5b5f5ae9c51cf43787c8ed13f0e312d1e2f1a
Michał Wiśniewski Front End Developer with backend skills <michalwisniewskifrontend@onet.pl>
2025-10-31 19:09:33 +0100

    Add CancelStripe component for payment failure message

---
+++ src/components/CancelStripe.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CancelStripe = () => {
+return (
+    <h1 style={{ textAlign: "center", marginTop: "50px" }}>
+      ❌ Płatność anulowana lub nieudana.
+    </h1>
+  );
+}
+
+export default CancelStripe
