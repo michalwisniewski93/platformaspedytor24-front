bcc5436dd4b9589c60198e4a4bd882969570ad6e
Michał Wiśniewski Front End Developer with backend skills <michalwisniewskifrontend@onet.pl>
2026-02-05 16:27:28 +0100

    Remove useState import from App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect} from 'react'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import {Provider} from 'react-redux'
 import store from './redux/store'
